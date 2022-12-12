fc7dd527fb9850857b3567409cf5d1c262b0e01c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-12 11:03:05 +0300

    incremnt/decreemnt event handler

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,1 +1,13 @@
-import React from ""
+import React from "react";
+
+export default function Event(){
+    const developerCLick = () =>{
+        alert("Good morning React Developer Kimaiyo")
+    };
+
+    return(
+        <div className="App">
+            <button onClick={developerCLick}>Click Me</button>
+        </div>
+    );
+}
